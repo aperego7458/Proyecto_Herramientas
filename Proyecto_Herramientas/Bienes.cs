012f9523dc41c72cde80d45fb34cdf3cd4765313
Andres Ferney Perez Gomez <ANDRESFER7458@hotmail.com>
2021-11-06 20:45:09 -0500

    ok

--- Proyecto_Herramientas/Bienes.cs
+++ Proyecto_Herramientas/Bienes.cs
@@ -15,22 +15,13 @@ namespace Proyecto_Herramientas
     {
         SqlConnection conn = new SqlConnection("server=DESKTOP-U6MQJK7;  database=ProyectoHerramienta; integrated security= true");
         CBienes bienes = new CBienes();
-        public int idBien = 0;
 
         public Bienes()
         {
             InitializeComponent();
             Leer();
         }
-        public void Limpiar()
-        {
-            txtPCiudad.Clear();
-            txtPDireccion.Clear();
-            txtChabitaciones.Clear();
-            txtCbanos.Clear();
-            txtPPrecio.Clear();
-            txtPFotos.Clear();
-        }
+        
         public void Leer()
         {
             conn.Open();
@@ -42,18 +33,12 @@ namespace Proyecto_Herramientas
             adapter.Fill(resultado);
             dgvBienes.DataSource = resultado;           
             conn.Close();
-            Limpiar();
         }
 
 
         private void label7_Click(object sender, EventArgs e)
         {
             //nuevo ingreso
-        }
-
-        private void btnAgregar_Click(object sender, EventArgs e)
-        {
-
         }
 
         private void btnAgregar_Click(object sender, EventArgs e)
@@ -70,7 +55,7 @@ namespace Proyecto_Herramientas
 
             bienes.GuardarBienes();
             MessageBox.Show("Registro completado!!");
-          
+
             Leer();
         }
 
@@ -82,6 +67,12 @@ namespace Proyecto_Herramientas
         private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
         {
             Leer();
+            bienes.Ciudad = dgvBienes.CurrentRow.Cells[1].Value.ToString();
+            bienes.Direccion = dgvBienes.CurrentRow.Cells[2].Value.ToString();
+            bienes.CHabitaciones = Convert.ToInt32(dgvBienes.CurrentRow.Cells[3].Value);
+            bienes.CBanos = Convert.ToInt32(dgvBienes.CurrentRow.Cells[4].Value);
+            bienes.Precio = Convert.ToInt32(dgvBienes.CurrentRow.Cells[5].Value);
+            bienes.Fotos = dgvBienes.   CurrentRow.Cells[6].Value.ToString();
         }
 
         private void dgvBienes_CellClick(object sender, DataGridViewCellEventArgs e)
@@ -90,8 +81,7 @@ namespace Proyecto_Herramientas
             {
                 DataGridViewRow fila = this.dgvBienes.Rows[e.RowIndex];
 
-                idBien = Convert.ToInt32(fila.Cells[0].Value);
-                txtIdPropiedad.Text = idBien.ToString();
+                
                 txtPCiudad.Text = fila.Cells[1].Value.ToString();
                 txtPDireccion.Text = fila.Cells[2].Value.ToString();
                 txtChabitaciones.Text = fila.Cells[3].Value.ToString();
@@ -100,68 +90,6 @@ namespace Proyecto_Herramientas
                 txtPFotos.Text = fila.Cells[6].Value.ToString();
                
             }
-        }
-
-        private void btnModificar_Click(object sender, EventArgs e)
-        {
-            bienes.Ciudad = txtPCiudad.Text;
-            bienes.Direccion = txtPDireccion.Text;
-            bienes.CHabitaciones = Convert.ToInt32(txtChabitaciones.Text);
-            bienes.CBanos = Convert.ToInt32(txtCbanos.Text);
-            bienes.Precio = Convert.ToInt32(txtPPrecio.Text);
-            bienes.Fotos = txtPFotos.Text;
-            bienes.IdBienes = idBien;
-
-            bienes.ActualizarBienes();
-            MessageBox.Show("Actualización correctamente!!");
-        
-            Leer();
-        }
-
-        private void label8_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void btnBuscar_Click(object sender, EventArgs e)
-        {
-            int i = 0, n = dgvBienes.Rows.Count - 1;
-
-            if (n > 0)
-            {
-                for(i =0; i < n ; i++)
-                {
-                    DataGridViewRow fila = this.dgvBienes.Rows[i];
-                    
-                    if(txtIdPropiedad.Text == fila.Cells[0].Value.ToString())
-                    {
-                        idBien = Convert.ToInt32(fila.Cells[0].Value);
-                        txtIdPropiedad.Text = idBien.ToString();
-                        txtPCiudad.Text = fila.Cells[1].Value.ToString();
-                        txtPDireccion.Text = fila.Cells[2].Value.ToString();
-                        txtChabitaciones.Text = fila.Cells[3].Value.ToString();
-                        txtCbanos.Text = fila.Cells[4].Value.ToString();
-                        txtPPrecio.Text = fila.Cells[5].Value.ToString();
-                        txtPFotos.Text = fila.Cells[6].Value.ToString();
-                        break;
-                    }
-                }
-                if (i == n)
-                {
-                    MessageBox.Show("ID no encontrado!!");
-                }
-            }
-        }
-
-        private void btnEliminar_Click(object sender, EventArgs e)
-        {
-            conn.Open();
-            string consulta = "DELETE FROM Propiedades WHERE ID_PROPIEDAD=@p1";
-            SqlCommand comando = new SqlCommand(consulta, conn);
-            comando.Parameters.AddWithValue("p1", idBien);
-            comando.ExecuteNonQuery();
-            conn.Close();
-            Leer();
         }
     }
 }
